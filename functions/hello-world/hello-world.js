f6c7387b88d4b752af2443b01bc49fd586fc2d6c
Stephen Pinkerton <stephen.pinkerton@datadoghq.com>
2021-08-31 13:53:45 -0700

    Add errors to api

--- functions/hello-world/hello-world.js
+++ functions/hello-world/hello-world.js
@@ -1,18 +1,26 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 const handler = async (event) => {
-  console.log("Running test function");
-  try {
-    const subject = event.queryStringParameters.name || 'World'
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
+  console.log("Starting user authentication")
+  if (Math.random() > 0.66) {
+      // internal server error
+      throw new Error("Critical error accessing user")
+      //  return { statusCode: 500, body: error.toString() }
     }
-  } catch (error) {
-    return { statusCode: 500, body: error.toString() }
-  }
+    if (Math.random() > 0.5) {
+      console.error("Unauthorized request for user")
+      return {
+        statusCode: 403,
+        body: JSON.stringify({ error: true, name: 'Null' }),
+        headers: {'Content-Type', 'application/json'},
+      }
+    } else {
+      console.log("Successfully located user")
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ err: false, name: 'John Doe' }),
+        headers: {'Content-Type', 'application/json'},
+      }
+    }
 }
 
 module.exports = { handler }
